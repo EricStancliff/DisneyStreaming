69c25fcca8db23d20c45257b8e84c87068255cbc
Eric <ericstanclif@gmail.com>
2022-01-11 16:10:35 -0600

    no validation.

--- src/main.cpp
+++ src/main.cpp
@@ -14,38 +14,11 @@
 #include "Background.h"
 #include "TileManager.h"
 
-struct VulkanWindow
+int main(int argc, char* argv[])
 {
-	vkl::Window window;
-	vkl::Surface surface;
-	vkl::Device device;
-	vkl::SwapChain swapChain;
-	vkl::RenderPass mainPass;
-	vkl::BufferManager bufferManager;
-	vkl::PipelineManager pipelineManager;
-	vkl::CommandDispatcher commandDispatcher;
-	std::vector<std::shared_ptr<vkl::RenderObject>> renderObjects;
+	vkl::Instance instance("disney_streaming", false);
 
-	void cleanUp(const vkl::Instance& instance)
-	{
-		device.waitIdle();
-		for (auto&& ro : renderObjects)
-			ro->cleanUp(device);
-		renderObjects.clear();
-		commandDispatcher.cleanUp(device);
-		pipelineManager.cleanUp(device);
-		bufferManager.cleanUp(device);
-		mainPass.cleanUp(device);
-		swapChain.cleanUp(device);
-		device.cleanUp();
-		surface.cleanUp(instance);
-		window.cleanUp();
-	}
-};
-
-VulkanWindow buildWindow(const vkl::Instance& instance, const std::string& title)
-{
-	vkl::Window window(1080, 720, title.c_str());
+	vkl::Window window(1080, 720, "Disney Streaming");
 	vkl::Surface surface(instance, window);
 	vkl::Device device(instance, surface);
 
@@ -65,68 +38,41 @@ VulkanWindow buildWindow(const vkl::Instance& instance, const std::string& title
 	vkl::PipelineManager pipelineManager(device, swapChain, mainPass);
 
 	vkl::CommandDispatcher commandDispatcher(device, swapChain);
+	std::vector<std::shared_ptr<vkl::RenderObject>> renderObjects;
 
-	return { std::move(window), std::move(surface), std::move(device), std::move(swapChain),
-		std::move(mainPass), std::move(bufferManager), std::move(pipelineManager), std::move(commandDispatcher),
-		{}
-	};
-}
 
-void updateWindow(VulkanWindow& window)
-{
-	window.bufferManager.update(window.device, window.swapChain);
-	window.commandDispatcher.processUnsortedObjects(window.renderObjects, window.device, window.pipelineManager, window.mainPass, window.swapChain, window.swapChain.frameBuffer(window.swapChain.frame()), window.swapChain.swapChainExtent());
-	window.swapChain.swap(window.device, window.surface, window.commandDispatcher, window.mainPass, window.window.getWindowSize());
-}
-
-bool seedRandom()
-{
-	std::srand((unsigned int)std::time(0));
-	return true;
-}
-float fRand(float fMin, float fMax)
-{
-	float f = (float)rand() / RAND_MAX;
-	return fMin + f * (fMax - fMin);
-}
-glm::vec3 randomPosition()
-{
-	static bool seeded = seedRandom();
-	if (seeded)
-	{
-		glm::vec3 position;
-		position.x = fRand(-50, 50);
-		position.y = 0;
-		position.z = fRand(-50, 50);
-		return position;
-	}
-	return {};
-}
-
-int main(int argc, char* argv[])
-{
-	vkl::Instance instance("disney_streaming", true);
-
-	VulkanWindow window = buildWindow(instance, "Disney Streaming");
-
-	auto bg = std::make_shared<Background>(window.device, window.swapChain, window.pipelineManager, window.bufferManager);
-	window.renderObjects.push_back(bg);
+	auto bg = std::make_shared<Background>(device, swapChain, pipelineManager, bufferManager);
+	renderObjects.push_back(bg);
 
 	TileManager mgr;
 
-	while (!window.window.shouldClose())
+	while (!window.shouldClose())
 	{
-		window.window.clearLastFrame();
+		window.clearLastFrame();
 		vkl::Window::pollEventsForAllWindows();
-		window.swapChain.prepNextFrame(window.device, window.surface, window.commandDispatcher, window.mainPass, window.window.getWindowSize());
+		swapChain.prepNextFrame(device, surface, commandDispatcher, mainPass, window.getWindowSize());
 
-		mgr.update(window.device, window.swapChain, window.pipelineManager, window.bufferManager, window.renderObjects, window.window);
+		mgr.update(device, swapChain, pipelineManager, bufferManager, renderObjects, window);
 
-		updateWindow(window);
+		bufferManager.update(device, swapChain);
+		commandDispatcher.processUnsortedObjects(renderObjects, device, pipelineManager, mainPass, swapChain, swapChain.frameBuffer(swapChain.frame()), swapChain.swapChainExtent());
+		swapChain.swap(device, surface, commandDispatcher, mainPass, window.getWindowSize());
 		//window.bufferManager.cleanUnusedBuffers(window.device);
 	}
 
-	window.cleanUp(instance);
+	device.waitIdle();
+	for (auto&& ro : renderObjects)
+		ro->cleanUp(device);
+	renderObjects.clear();
+	commandDispatcher.cleanUp(device);
+	pipelineManager.cleanUp(device);
+	bufferManager.cleanUp(device);
+	mainPass.cleanUp(device);
+	swapChain.cleanUp(device);
+	device.cleanUp();
+	surface.cleanUp(instance);
+	window.cleanUp();
+
 	instance.cleanUp();
 	vkl::Window::cleanUpWindowSystem();
 }
